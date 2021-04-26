2b7c8a88fc161dc3dc13a445d4a045b3d9a2aac2
Nhat Thanh <thanhnhatcm@gmail.com>
2021-04-26 20:08:30 +0700

    ok

--- ExternalSort/main.js
+++ ExternalSort/main.js
@@ -4,8 +4,12 @@ var app = new Vue({
     inputArrStr:
       "17 37 3 7 41 62 15 38 23 89 77 5 19 16 18 42 24 8 55 65 56 88 1 10 60 36 12 48 26 80 99 54 6 49 74 94 50 86 32 2 90 30 63 75 44 57 34 76 61 25 13 51 81 100 47 82 35 69 4 72 46 33 27 79 68 52 53 91 59 70 64 11 31 73 71 22 98 92 39 96 9 40 58 97 43 78 83 95 45 66 28 84 21 20 14 85 87 93 29 67",
     run_size: 8,
-    splitArr: [],
+    blockArr: [],
+    colorsArr: [],
+    split_size: 1,
+    block_size: 1,
     outputArr: [],
+    outputColorsArr: [],
     sorted: false,
     splitM: -1,
     splitMV: -1,
@@ -19,16 +23,30 @@ var app = new Vue({
     split() {
       this.sorted = false;
       this.outputArr = [];
-      this.splitArr = [];
+      this.colorsArr = [];
+      this.blockArr = [];
+      for (let i = 0; i < this.block_size; i++) {
+        this.blockArr.push([]);
+        this.colorsArr.push(
+          "#" + Math.floor(Math.random() * 16777215).toString(16)
+        );
+      }
+      let splitIndex = 0;
+      let splitCount = 0;
       for (let i = 0; i < this.inputArr.length; i++) {
-        if (i % this.run_size == 0) this.splitArr.push([]);
-        this.splitArr[this.splitArr.length - 1].push(this.inputArr[i]);
+        this.blockArr[splitIndex].push(this.inputArr[i]);
+        splitCount++;
+        if (splitCount == this.split_size) {
+          splitCount = 0;
+          splitIndex++;
+        }
+        if (splitIndex == this.block_size) splitIndex = 0;
       }
     },
     sortSplitArr() {
       this.sorted = true;
-      for (let i = 0; i < this.splitArr.length; i++)
-        this.splitArr[i].sort(function (a, b) {
+      for (let i = 0; i < this.blockArr.length; i++)
+        this.blockArr[i].sort(function (a, b) {
           return a - b;
         });
     },
@@ -36,17 +54,18 @@ var app = new Vue({
       this.splitM = -1;
       this.splitMV = 1e9;
       let check = false;
-      for (let i = 0; i < this.splitArr.length; i++) {
-        if (this.splitArr[i].length > 0)
-          if (this.splitArr[i][0] < this.splitMV) {
+      for (let i = 0; i < this.blockArr.length; i++) {
+        if (this.blockArr[i].length > 0)
+          if (this.blockArr[i][0] < this.splitMV) {
             this.splitM = i;
-            this.splitMV = this.splitArr[i][0];
+            this.splitMV = this.blockArr[i][0];
             check = true;
           }
       }
       if (check) {
+        this.blockArr[this.splitM].splice(0, 1);
         this.outputArr.push(this.splitMV);
-        this.splitArr[this.splitM].shift();
+        this.outputColorsArr.push(this.colorsArr[this.splitM]);
       }
     },
   },
